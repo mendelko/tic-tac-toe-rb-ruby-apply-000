d626a8a26c25a9593941527a7d7f4acf3cb5d267
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-11-11 22:08:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -64,14 +64,14 @@ end
 
 
 def turn(board)
-   puts "Please enter 1-9:"
-   user_input = gets.strip
-   index = input_to_index(user_input)
-   if valid_move?(board, index)
-      move(board, index, current_player(board))
+  puts "Please enter 1-9:"
+    userInput = gets.strip 
+    if !valid_move?(board, index) 
       turn(board)
-   end
-   display_board(board)
+    else 
+      move(board, index, current_player(board))
+    end
+  display_board(board)
 end
 
 
