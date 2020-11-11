60bcf93b15c1f9be9aff07c208fe135660e2e0ff
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-11-11 21:56:51 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -92,3 +92,33 @@ def won?(board)
   end
   return false
 end
+
+
+def full?(board)
+   board.all? {|i| i == "X" || i == "O"}
+end
+
+
+def draw?(board)
+   if !won?(board) && full?(board)
+     return true
+   elsif !won?(board) && !full?(board)
+     return false
+   else won?(board)
+     return false
+   end
+end
+
+
+def over?(board)
+   if draw?(board) || won?(board) || full?(board)
+     return true
+   end
+end
+
+
+def winner(board)
+   if won?(board)
+      return board[won?(board)[0]]
+   end
+end
