9f3018722f2c8d60cb62f49fa293602beafae46b
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-11-11 21:47:40 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,13 +18,16 @@ def display_board(board)
    puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+
 def input_to_index(user_input)
    user_input.to_i - 1
 end
 
+
 def move(board, index, player)
    board[index] = player
 end
+
 
 def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
@@ -34,10 +37,22 @@ def position_taken?(board, index)
    end
 end
 
+
 def valid_move?(board, index)
   if !position_taken?(board, index) && (index).between?(0,8)
     return true
   else
     return false
   end
+end
+
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
+  }
+  counter
 end
