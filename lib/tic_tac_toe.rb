6e8d2116886d06e60ac85b95dfad57fdd19575d7
Menachem Mendel Konikov <mkonikov123@gmail.com>
2020-11-11 21:45:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,3 +33,11 @@ def position_taken?(board, index)
       return true
    end
 end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else 
+    return false
+  end
+end
